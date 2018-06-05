835dd37263a5973285a02a9ccc430a66585a7ae3
Christian Pauly <christian-pauly@posteo.de>
2018-06-05 17:37:59 +0200

    Removing emblem counter, when there are no new messages

--- fluffychat-pushgateway.js
+++ fluffychat-pushgateway.js
@@ -3,14 +3,12 @@ const request = require('request');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('//etc/letsencrypt/live/janian.de/privkey.pem'),
-  cert: fs.readFileSync('//etc/letsencrypt/live/janian.de/fullchain.pem'),
+    key: fs.readFileSync('//etc/letsencrypt/live/janian.de/privkey.pem'),
+    cert: fs.readFileSync('//etc/letsencrypt/live/janian.de/fullchain.pem'),
 };
 
 app.createServer( options,
     function (req, res) {
-res.writeHead(200);
-  res.end("Fluffychat Push-Gateway");
         if (req.method == 'POST') {
             var jsonString = '';
 
@@ -20,52 +18,64 @@ res.writeHead(200);
 
             req.on('end', function () {
                 notification = JSON.parse(jsonString).notification;
-                console.log("New Notification received at: " + new Date());
 
                 var approxExpire = new Date ();
                 approxExpire.setUTCMinutes(approxExpire.getUTCMinutes()+10);
 
                 var devices = notification.devices;
+                var rejected = [];
                 for ( var i = 0; i < devices.length; i++ ) {
 
-		// Workaround for wrong appid:
-		if ( devices[i].app_id === "fluffychat.christianpauly_hello" ) devices[i].app_id = "fluffychat.christianpauly_fluffychat";
+                    // Workaround for wrong appid:
+                    if ( devices[i].app_id === "fluffychat.christianpauly_hello" ) devices[i].app_id = "fluffychat.christianpauly_fluffychat";
 
-                    if ( devices[i].app_id === "fluffychat.christianpauly_fluffychat" && notification.type === "m.room.message" ) {
-			var room = notification.room_name || notification.sender_display_name || notification.sender;
-                        data = {
+                    var localPushKey = devices[i].pushkey;
+
+                    if ( devices[i].app_id === "fluffychat.christianpauly_fluffychat" ) {
+                        var room = notification.room_name || notification.sender_display_name || notification.sender || "";
+                        console.log ( room );
+                        var isMessage = notification.type === "m.room.message"
+
+                        var notifydata = {
                             "appid" : devices[i].app_id,
                             "expire_on": approxExpire.toISOString(),
-                            "token": devices[i].pushkey,
+                            "token": localPushKey,
                             "clear_pending": true,
                             "replace_tag": room,
                             "data": {
-                                "message": "New Message",
+                                "message": notification,
                                 "notification": {
-                                    "card": {
-					"action": "application:///fluffychat.desktop",
-                                        "summary": notification.room_name || notification.sender_display_name || notification.sender,
-                                        "body": notification.content ? notification.content.body : "New secret Message",
-					"persist": true,
-                                        "popup": true
-                                    },
-
-					"sound": true,
+                                    "sound": isMessage,
                                     "tag": room,
-                                    "vibrate": true,
+                                    "vibrate": isMessage,
                                     "emblem-counter": {
-                                        "count": notification.counts.unread || 1,
-                                        "visible": true
+                                        "count": notification.counts.unread || 0,
+                                        "visible": (notification.counts.unread || 0) > 0 ? true : false
                                     }
                                 }
                             }
-
                         };
-                        request.post("https://push.ubports.com/notify", {json: data}, function(err,res,body) {console.log(body)});
+
+                        if ( isMessage ) notifydata.data.notification.card = {
+                            "action": "application:///fluffychat.desktop",
+                            "summary": notification.room_name || notification.sender_display_name || notification.sender,
+                            "body": notification.content ? notification.content.body : "New secret Message",
+                            "persist": true,
+                            "popup": true
+                        };
+                        request.post("https://push.ubports.com/notify", {json: notifydata}, function(err,res,body) {
+                            rejected.push(localPushKey);
+                            //res.writeHead(200);
+                            //res.end(rejected);
+                        });
                     }
                 }
 
             });
         }
+        else {
+            res.writeHead(200);
+            res.end("Fluffychat Push-Gateway");
+        }
     }
-).listen(7000);
+).listen(7001);
